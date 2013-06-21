eeb225dc2d3ef073ad165dd3e66d075cb0ddef63
mlinhard <michal@linhard.sk>
2013-06-21 13:39:30 +0200

    add some comments and one method

--- model/src/main/java/org/jboss/qa/perfrepo/model/builder/TestBuilder.java
+++ model/src/main/java/org/jboss/qa/perfrepo/model/builder/TestBuilder.java
@@ -16,48 +16,106 @@ import org.jboss.qa.perfrepo.model.TestMetric;
 public class TestBuilder {
    private Test test;
 
+   /**
+    * Create new {@link TestBuilder}.
+    */
    public TestBuilder() {
       test = new Test();
    }
 
+   /**
+    * Set the test name.
+    * 
+    * @param name
+    * @return this {@link TestBuilder}
+    */
    public TestBuilder name(String name) {
       test.setName(name);
       return this;
    }
 
+   /**
+    * Set the test description.
+    * 
+    * @param description
+    * @return this {@link TestBuilder}
+    */
    public TestBuilder description(String description) {
       test.setDescription(description);
       return this;
    }
 
+   /**
+    * Set the test groupId.
+    * 
+    * @param groupId
+    * @return this {@link TestBuilder}
+    */
    public TestBuilder groupId(String groupId) {
       test.setGroupId(groupId);
       return this;
    }
 
+   /**
+    * Set the test uid.
+    * 
+    * @param uid
+    * @return this {@link TestBuilder}
+    */
    public TestBuilder uid(String uid) {
       test.setUid(uid);
       return this;
    }
 
+   /**
+    * Creates a new {@link Test} based on defined values.
+    * 
+    * @return the {@link Test}
+    */
    public Test build() {
       Test ret = test;
       test = new Test();
       return ret;
    }
 
+   /**
+    * Shortcut for metric().name(name).comparator(comparator).description(description).test()
+    * 
+    * @param name
+    * @param comparator
+    * @param description
+    * @return this {@link TestBuilder}
+    */
    public TestBuilder metric(String name, String comparator, String description) {
-      return new MetricBuilder(this, addMetric(new Metric())).name(name).comparator(comparator).description(description).test();
+      return metric().name(name).comparator(comparator).description(description).test();
    }
 
+   /**
+    * Shortcut for metric().name(name).description(description).test()
+    * 
+    * @param name
+    * @param description
+    * @return this {@link TestBuilder}
+    */
    public TestBuilder metric(String name, String description) {
-      return new MetricBuilder(this, addMetric(new Metric())).name(name).description(description).test();
+      return metric().name(name).description(description).test();
    }
 
+   /**
+    * Shortcut for metric().name(name).test()
+    * 
+    * @param name
+    * @return this {@link TestBuilder}
+    */
    public TestBuilder metric(String name) {
-      return new MetricBuilder(this, addMetric(new Metric())).name(name).test();
+      return metric().name(name).test();
    }
 
+   /**
+    * Add a new {@link Metric}.
+    * 
+    * @return new {@link MetricBuilder}
+    */
    public MetricBuilder metric() {
       return new MetricBuilder(this, addMetric(new Metric()));
    }
--- model/src/main/java/org/jboss/qa/perfrepo/model/builder/TestExecutionBuilder.java
+++ model/src/main/java/org/jboss/qa/perfrepo/model/builder/TestExecutionBuilder.java
@@ -21,15 +21,30 @@ import org.jboss.qa.perfrepo.model.Value;
 public class TestExecutionBuilder {
    private TestExecution testExecution;
 
+   /**
+    * Create a new {@link TestExecutionBuilder}.
+    */
    public TestExecutionBuilder() {
       this.testExecution = new TestExecution();
    }
 
+   /**
+    * Set the name.
+    * 
+    * @param name
+    * @return this {@link TestExecutionBuilder}
+    */
    public TestExecutionBuilder name(String name) {
       testExecution.setName(name);
       return this;
    }
 
+   /**
+    * Set the started date.
+    * 
+    * @param name
+    * @return this {@link TestExecutionBuilder}
+    */
    public TestExecutionBuilder started(Date started) {
       testExecution.setStarted(started);
       return this;
@@ -49,6 +64,14 @@ public class TestExecutionBuilder {
       return addParameter(new TestExecutionParameter(name, value));
    }
 
+   /**
+    * Add a new {@link TestExecutionParameter} to this {@link TestExecution} with given name and
+    * value
+    * 
+    * @param name
+    * @param value
+    * @return this {@link TestExecutionBuilder}
+    */
    public TestExecutionBuilder parameter(String name, String value) {
       addParameter(name, value);
       return this;
@@ -68,6 +91,12 @@ public class TestExecutionBuilder {
       return tagObj;
    }
 
+   /**
+    * Add a new {@link Tag} to this {@link TestExecution} with given name
+    * 
+    * @param tag
+    * @return
+    */
    public TestExecutionBuilder tag(String tag) {
       addTag(tag);
       return this;
@@ -83,18 +112,47 @@ public class TestExecutionBuilder {
       return value;
    }
 
+   /**
+    * Add a new {@link Value}
+    * 
+    * @return new {@link ValueBuilder}
+    */
    public ValueBuilder value() {
       return new ValueBuilder(this, addValue(new Value()));
    }
 
+   /**
+    * Shortcut for value().metricName(metricName).resultValue(value).parameter(paramName,
+    * paramValue).execution()
+    * 
+    * @param metricName
+    * @param value
+    * @param paramName
+    * @param paramValue
+    * @return this {@link TestExecutionBuilder}
+    */
    public TestExecutionBuilder value(String metricName, Double value, String paramName, String paramValue) {
       return value().metricName(metricName).resultValue(value).parameter(paramName, paramValue).execution();
    }
 
+   /**
+    * Shortcut for value().metricName(metricName).resultValue(value).execution()
+    * 
+    * @param metricName
+    * @param value
+    * @return this {@link TestExecutionBuilder}
+    */
    public TestExecutionBuilder value(String metricName, Double value) {
       return value().metricName(metricName).resultValue(value).execution();
    }
 
+   /**
+    * Sets the id of the {@link Test} referenced by this {@link TestExecution}. If the test is not
+    * present, it's created.
+    * 
+    * @param testId
+    * @return this {@link TestExecutionBuilder}
+    */
    public TestExecutionBuilder testId(Long testId) {
       Test test = testExecution.getTest();
       if (test == null) {
@@ -105,6 +163,28 @@ public class TestExecutionBuilder {
       return this;
    }
 
+   /**
+    * Sets the uid of the {@link Test} referenced by this {@link TestExecution}. If the test is not
+    * present, it's created.
+    * 
+    * @param testUid
+    * @return this {@link TestExecutionBuilder}
+    */
+   public TestExecutionBuilder testUid(String testUid) {
+      Test test = testExecution.getTest();
+      if (test == null) {
+         test = new Test();
+         testExecution.setTest(test);
+      }
+      test.setUid(testUid);
+      return this;
+   }
+
+   /**
+    * Creates a new {@link TestExecution} based on defined values.
+    * 
+    * @return the {@link TestExecution}
+    */
    public TestExecution build() {
       TestExecution r = testExecution;
       testExecution = new TestExecution();
