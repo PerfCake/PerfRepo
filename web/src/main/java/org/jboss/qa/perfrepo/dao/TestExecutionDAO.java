bfff2fc32bfd59c6853610a47f44def03613daa3
mlinhard <michal@linhard.sk>
2013-12-21 01:51:57 +0100

    add comment field to groupBy clause

--- web/src/main/java/org/jboss/qa/perfrepo/dao/TestExecutionDAO.java
+++ web/src/main/java/org/jboss/qa/perfrepo/dao/TestExecutionDAO.java
@@ -197,7 +197,7 @@ public class TestExecutionDAO extends DAO<TestExecution, Long> {
       criteria.having(pHavingAllTagsPresent);
       // this isn't very ellegant, but Postgres 8.4 doesn't allow GROUP BY only with id
       // this feature is allowed only since Postgres 9.1+
-      criteria.groupBy(rExec.get("test"), rExec.get("id"), rExec.get("name"), rExec.get("locked"), rExec.get("started"), rExec.get("jobId"));
+      criteria.groupBy(rExec.get("test"), rExec.get("id"), rExec.get("name"), rExec.get("locked"), rExec.get("started"), rExec.get("jobId"), rExec.get("comment"));
       TypedQuery<TestExecution> query = query(criteria);
 
       // set parameters
