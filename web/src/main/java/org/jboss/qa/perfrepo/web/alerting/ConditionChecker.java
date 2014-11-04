bebe19cc4173d7f4156a05a46541be1ebb24c78c
Jiri Holusa <jholusa@redhat.com>
2014-11-04 10:23:44 +0100

    Initial commit for alerting feature [PERFREPO-93]

---
+++ web/src/main/java/org/jboss/qa/perfrepo/web/alerting/ConditionChecker.java
@@ -0,0 +1,19 @@
+package org.jboss.qa.perfrepo.web.alerting;
+
+/**
+ * API for checking of condition for alerting.
+ *
+ * @author Jiri Holusa (jholusa@redhat.com)
+ */
+public interface ConditionChecker {
+
+   /**
+    * Checks the condition for alerting
+    *
+    * @param condition specification of condition in String in our custom DSL that should hold
+    * @param currentResult result of the test execution that is currently being processed, e.g. against which the
+    *                      condition should hold
+    * @return true if condition still hold | false if condition is broken
+    */
+   public boolean checkCondition(String condition, double currentResult);
+}
