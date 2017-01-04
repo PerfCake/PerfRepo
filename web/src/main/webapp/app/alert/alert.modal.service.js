d4065514db4fb20700f7edbcab43227fb5ee8f5d
Jiri Grunwald <grunwjir@gmail.com>
2017-01-05 00:09:25 +0100

    [frontend] Refactoring - Added metric and alert service. It contains methods for creating forms in a modal window.

---
+++ web/src/main/webapp/app/alert/alert.modal.service.js
@@ -0,0 +1,75 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('org.perfrepo.alert')
+        .service('alertModalService', AlertModalService);
+
+    function AlertModalService($modal) {
+
+        return {
+            createAlert: createAlert,
+            editAlert: editAlert,
+            removeAlert: removeAlert
+        };
+
+        function removeAlert(alert) {
+            return $modal.open({
+                animation: true,
+                backdrop: 'static',
+                keyboard: false,
+                templateUrl: 'app/alert/remove_modal/remove-alert.view.html',
+                controller: 'RemoveAlertController',
+                controllerAs: 'vm',
+                size: 'sm',
+                resolve : {
+                    _alert: function() {
+                        return alert;
+                    }
+                }
+            });
+        }
+
+        function createAlert(testMetrics, testId) {
+            return $modal.open({
+                animation: true,
+                backdrop: 'static',
+                keyboard: false,
+                templateUrl: 'app/alert/create_modal/create-alert.view.html',
+                controller: 'CreateAlertController',
+                controllerAs: 'vm',
+                size: 'md',
+                resolve : {
+                    _metrics: function() {
+                        return testMetrics;
+                    },
+                    _testId: function () {
+                        return testId;
+                    }
+                }
+            });
+        }
+
+        function editAlert(testMetrics, alertId) {
+            return $modal.open({
+                animation: true,
+                backdrop: 'static',
+                keyboard: false,
+                templateUrl: 'app/alert/edit_modal/edit-alert.view.html',
+                controller: 'EditAlertController',
+                controllerAs: 'vm',
+                size: 'md',
+                resolve : {
+                    _metrics: function() {
+                        return testMetrics;
+                    },
+                    _alert: function (alertService) {
+                        return alertService.getById(alertId);
+                    }
+                }
+            });
+        }
+
+
+    }
+})();
--- web/src/main/webapp/app/test/detail/detail-test.controller.js
+++ web/src/main/webapp/app/test/detail/detail-test.controller.js
@@ -11,7 +11,8 @@
         ])
         .controller('DetailTestController', DetailTestController);
 
-    function DetailTestController(_test, _isUserAlertsSubscriber, testService, testModalService, $modal) {
+    function DetailTestController(_test, _isUserAlertsSubscriber, testService, testModalService,
+                                  metricModalService, alertModalService) {
         var vm = this;
         vm.test = _test;
         vm.isUserAlertsSubscriber = _isUserAlertsSubscriber;
@@ -53,155 +54,50 @@
         }
 
         function addAlert() {
-            var modalInstance = $modal.open({
-                animation: true,
-                backdrop: 'static',
-                keyboard: false,
-                templateUrl: 'app/alert/create_modal/create-alert.view.html',
-                controller: 'CreateAlertController',
-                controllerAs: 'vm',
-                size: 'md',
-                resolve : {
-                    _metrics: function() {
-                        return vm.test.metrics;
-                    },
-                    _testId: function () {
-                        return vm.test.id;
-                    }
-                }
-            });
+            var modalInstance = alertModalService.createAlert(vm.test.metrics, vm.test.id);
 
             modalInstance.result.then(function () {
                 updateDetail();
-            }, function () {
-                console.log('Modal dismissed');
             });
         }
 
         function editAlert(alert) {
-            var modalInstance = $modal.open({
-                animation: true,
-                backdrop: 'static',
-                keyboard: false,
-                templateUrl: 'app/alert/edit_modal/edit-alert.view.html',
-                controller: 'EditAlertController',
-                controllerAs: 'vm',
-                size: 'md',
-                resolve : {
-                    _metrics: function() {
-                        return vm.test.metrics;
-                    },
-                    _alert: function (alertService) {
-                        return alertService.getById(alert.id);
-                    }
-                }
-            });
+            var modalInstance = alertModalService.editAlert(vm.test.metrics, alert.id);
 
             modalInstance.result.then(function () {
                 updateDetail();
-            }, function () {
-                console.log('Modal dismissed');
             });
         }
 
         function removeAlert(alert) {
-            var modalInstance = $modal.open({
-                animation: true,
-                backdrop: 'static',
-                keyboard: false,
-                templateUrl: 'app/alert/remove_modal/remove-alert.view.html',
-                controller: 'RemoveAlertController',
-                controllerAs: 'vm',
-                size: 'sm',
-                resolve : {
-                    _alert: function() {
-                        return alert;
-                    }
-                }
-            });
+            var modalInstance = alertModalService.removeAlert(alert);
 
             modalInstance.result.then(function () {
                 updateDetail();
-            }, function () {
-                console.log('Modal dismissed');
             });
         }
 
         function addMetric() {
-            var modalInstance = $modal.open({
-                animation: true,
-                backdrop: 'static',
-                keyboard: false,
-                templateUrl: 'app/metric/create_modal/create-metric.view.html',
-                controller: 'CreateMetricController',
-                controllerAs: 'vm',
-                size: 'md',
-                resolve : {
-                    _comparators: function(metricService) {
-                        return metricService.getComparators();
-                    },
-                    _testId: function () {
-                        return vm.test.id;
-                    }
-                }
-            });
+            var modalInstance = metricModalService.addOrCreateMetric(vm.test.id);
 
             modalInstance.result.then(function () {
                 updateDetail();
-            }, function () {
-                console.log('Modal dismissed');
             });
         }
 
         function editMetric(metric) {
-            var modalInstance = $modal.open({
-                animation: true,
-                backdrop: 'static',
-                keyboard: false,
-                templateUrl: 'app/metric/edit_modal/edit-metric.view.html',
-                controller: 'EditMetricController',
-                controllerAs: 'vm',
-                size: 'md',
-                resolve : {
-                    _comparators: function(metricService) {
-                        return metricService.getComparators();
-                    },
-                    _metric: function (metricService) {
-                        return metricService.getById(metric.id);
-                    }
-                }
-            });
+            var modalInstance = metricModalService.editMetric(metric.id);
 
             modalInstance.result.then(function () {
                 updateDetail();
-            }, function () {
-                console.log('Modal dismissed');
             });
         }
 
         function removeMetric(metric) {
-            var modalInstance = $modal.open({
-                animation: true,
-                backdrop: 'static',
-                keyboard: false,
-                templateUrl: 'app/metric/remove_modal/remove-metric.view.html',
-                controller: 'RemoveMetricController',
-                controllerAs: 'vm',
-                size: 'sm',
-                resolve : {
-                    _metric: function() {
-                        return metric;
-                    },
-                    _testId: function () {
-                        return vm.test.id;
-                    }
-                }
-            });
+            var modalInstance = metricModalService.removeMetric(metric, vm.test.id);
 
             modalInstance.result.then(function () {
                 updateDetail();
-            }, function () {
-                console.log('Modal dismissed');
             });
         }
 
@@ -210,8 +106,6 @@
 
             modalInstance.result.then(function () {
                 updateDetail();
-            }, function () {
-                console.log('Modal dismissed');
             });
         }
 
